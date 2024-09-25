e6b92a7371b8199821f10c0999c0334df3ea4c2e
JerrySimba <salibursalibur3@gmail.com>
2024-09-25 12:52:05 +0300

    adding data to "sql_intro" database

---
+++ insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO sql_intro.players (name, country, goals) 
+VALUES
+('Lionel Messi', 'Argentina', 10),
+('Cristiano Ronaldo', 'Portugal', 12),
+('Neymar Jr', 'Brazil', 9),
+('Kylian Mbappe', 'France', 11),
+('Mohamed Salah', 'Egypt', 8),
+('Robert Lewandowski', 'Poland', 10),
+('Kevin De Bruyne', 'Belgium', 7),
+('Sadio Mane', 'Senegal', 9),
+('Harry Kane', 'England', 8),
+('Erling Haaland', 'Norway', 11);
