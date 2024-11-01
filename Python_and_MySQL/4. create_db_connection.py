c156db095c98f9b118f6548af5b6d29aa4d63723
JerrySimba <salibursalibur3@gmail.com>
2024-11-02 01:14:18 +0300

    Create 4. create_db_connection.py

---
+++ Python_and_MySQL/4. create_db_connection.py
@@ -0,0 +1,12 @@
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password,
+            database = db_name)
+        print("mysql database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+    return connection
